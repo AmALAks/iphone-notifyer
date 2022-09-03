4df45352cd74e03dcfe4933999107af511f7cfed
Jonek <67602412+AmALAks@users.noreply.github.com>
2022-09-03 19:35:28 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const { Server } = require('ws');
 const wss = new Server({ server });
 const WindowsToaster = require('node-notifier').WindowsToaster;
 
+const local_ip = 192.168.0.112
+
 const notifier = new WindowsToaster({
   withFallback: true,
   customPath: undefined
@@ -21,5 +23,5 @@ wss.on('connection', (ws) => {
   ws.on('message', (msg) => notifier.notify({title: "IPhone", message: msg.toString("utf-8"), appID: "Notification", icon: __dirname}))
 });
 
-server.listen(8000, '192.168.0.112', () => {
+server.listen(8000, local_ip, () => {
 });
